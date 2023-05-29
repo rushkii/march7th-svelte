244ff39f8cb94a11ae68635505b55edb665e80e4
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-05-29 14:52:45 +0700

    layout: use `<Navigation/>` component inside `routes/+layout.svelte`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
   import "../app.css";
+  import Navigation from "../components/Navigation.svelte";
+  import { page } from "$app/stores";
 </script>
 
 
+<Navigation show={!($page.route.id === "/")}/>
 <main class="flex flex-col items-center min-h-screen text-white">
   <slot />
 </main>
