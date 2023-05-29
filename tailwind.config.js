7111ccd825d652e095e16744a2a044f55287b35c
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-05-29 07:53:52 +0700

    tailwind: add `screens` config
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    screens: {
+      "sm": "320px",
+      "md": "640px",
+      "lg": "768px",
+      "2lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px"
+    },
     extend: {},
   },
   plugins: [],
