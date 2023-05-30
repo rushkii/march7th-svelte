bf228b732f39aaee44f2a4bb6455de464e5fbec5
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-05-31 02:41:59 +0700

    components: add `<NavLink/>` component
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ src/components/NavLink.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  export let href = "";
+  export let name = "";
+  export let icon = "";
+</script>
+
+
+{#if !($page.url.pathname === href)}
+  <a
+    href={href}
+    class="flex items-center text-sm gap-2 p-2 transition-opacity hover:opacity-100 {$page.url.pathname === href ? 'opacity-100': 'opacity-70'}"
+  >
+    <div class="shrink-0">
+      <img src={icon} alt="" class="w-6 h-6" />
+    </div>
+    <div class="text-base overflow-hidden font-medium">
+      {name}
+    </div>
+  </a>
+{:else}
+  <div
+    class="flex items-center text-sm gap-2 p-2 cursor-pointer transition-opacity hover:opacity-100 {$page.url.pathname === href ? 'opacity-100': 'opacity-70'}"
+  >
+    <div class="shrink-0">
+      <img src={icon} alt="" class="w-6 h-6" />
+    </div>
+    <div class="text-base overflow-hidden font-medium">
+      {name}
+    </div>
+  </div>
+{/if}
