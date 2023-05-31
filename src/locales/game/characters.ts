fa255217b548146314529ba40a03407e6676f1c6
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-05-31 10:45:05 +0700

    locales: set characters data to the svelte store
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ src/locales/game/characters.ts
@@ -0,0 +1,13 @@
+import { locale } from '$lib/translations';
+import type { CharactersData } from '$types';
+import { writable } from 'svelte/store';
+
+
+const charStore = writable<CharactersData>();
+
+locale.subscribe(async (lang) => {
+  const charData: CharactersData = (await import(`./characters/${lang}.json`)).default;
+  charStore.set(charData);
+})
+
+export default charStore;
