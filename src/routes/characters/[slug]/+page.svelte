6b7b2b59af56364ca6b61d74ce988c51d9c6c0fb
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-06-03 04:14:51 +0700

    routes: characters: add character detail page
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ src/routes/characters/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let data;
+  console.log(data)
+</script>
+
+
+  <div
+    class="capitalize w-fit flex flex-col items-center justify-center px-5 py-2 font-bold text-xl 2lg:text-4xl bg-gradient-to-r from-sky-400 via-blue-400 to-pink-300 from-25% via-50% to-90%"
+    style="background-clip: text; -webkit-background-clip: text; -webkit-text-fill-color: transparent;"
+  >
+    {data.slug}
+    <div class="w-14 2lg:w-16 mt-1 bg-gradient-to-r from-sky-400 via-blue-400 to-pink-300 from-25% via-50% to-100% h-1 rounded-sm"/>
+  </div>
---
+++ src/routes/characters/[slug]/+page.ts
@@ -0,0 +1,18 @@
+import characters from '$locales/game/characters';
+import { error } from '@sveltejs/kit';
+import { get } from 'svelte/store';
+import type { PageLoad } from './$types';
+
+
+export const load: PageLoad = async ({ params }) => {
+	const slug = params.slug;
+	const character = get(characters)
+    .characters
+    .find((char) => (char.slugified === slug));
+
+	if (character === undefined) {
+		throw error(404, {message: `Looks like the character <b>${slug}</b> you are looking for is doesn't exist.` });
+	}
+
+	return { slug };
+};
