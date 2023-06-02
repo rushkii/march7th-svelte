a84c3d0b5eb94ff9f3c1a4bec3a85eb8498cc66b
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-06-03 04:15:24 +0700

    components: change styling statement to check current path
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- src/components/NavLink.svelte
+++ src/components/NavLink.svelte
@@ -10,7 +10,7 @@
   <a
     on:click={() => {}}
     href={nav.link}
-    class="hidden 2lg:flex items-center text-sm gap-2 p-2 transition-opacity hover:opacity-100 {$page.url.pathname === nav.link ? 'opacity-100': 'opacity-70'}"
+    class="hidden 2lg:flex items-center text-sm gap-2 p-2 transition-opacity hover:opacity-100 {$page.url.pathname.includes(nav.link) ? 'opacity-100': 'opacity-70'}"
   >
     <div class="shrink-0">
       <img src={nav.icon} alt="" class="w-6 h-6" />
@@ -23,7 +23,7 @@
     on:click={() => {}}
     href={nav.link}
     style="transition-delay: {i + 1}00ms"
-    class="flex 2lg:hidden items-center border border-gray-800 duration-300 bg-[#2524744b] text-lg w-48 p-3 rounded-lg { navOpen && $page.url.pathname === nav.link ? 'bg-[#252474ae] border-gray-700 opacity-100 shadow-md shadow-[#0000006f]' : navOpen && "opacity-70" } {!navOpen && 'opacity-0 translate-x-40 overflow-hidden'}"
+    class="flex 2lg:hidden items-center border border-gray-800 duration-300 bg-[#2524744b] text-lg w-48 p-3 rounded-lg { navOpen && $page.url.pathname.includes(nav.link) ? 'bg-[#252474ae] border-gray-700 opacity-100 shadow-md shadow-[#0000006f]' : navOpen && "opacity-70" } {!navOpen && 'opacity-0 translate-x-40 overflow-hidden'}"
   >
     <img src={nav.icon} alt="" class="h-6 w-6 mr-3" />
     <div
